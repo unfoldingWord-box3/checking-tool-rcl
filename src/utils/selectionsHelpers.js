9a86bee1f4d1bc3d510bfdfa25e7ff906198749b
PhotoNomad0 <bruce.mclean@unfoldingword.org>
2024-11-08 09:23:07 -0500

    work on selections checking

--- src/utils/selectionsHelpers.js
+++ src/utils/selectionsHelpers.js
@@ -1,11 +1,14 @@
 import usfm from 'usfm-js';
 import { checkSelectionOccurrences } from 'selections';
 import isEqual from 'deep-equal'
+import { getUsfmForVerseContent } from '../helpers/UsfmFileConversionHelpers'
+import { getVerses } from 'bible-reference-range'
+import { delay } from '../helpers/utils'
 
 /**
  *
  * @param {String} filteredTargetVerse - target bible verse.
- * @param {array}  selections - array of selection objects [Obj,...]
+ * @param {array} selections - array of selection objects [Obj,...]
  * @returns {boolean}
  */
 export function validateVerseSelections(filteredTargetVerse, selections) {
@@ -16,13 +19,13 @@ export function validateVerseSelections(filteredTargetVerse, selections) {
 
 /**
  * verify all selections for current verse
- * @param {string} targetVerse - new text for verse
+ * @param {string|object} targetVerse - new text for verse
  * @param {string} chapter
  * @param {string} verse
  * @param {string} bookId
  * @param {Object[]} groupsIndex - array of all the checks
  * @param {Function} invalidateCheckCallback
- * @return {Function}
+ * @return {boolean}
  */
 export const validateAllSelectionsForVerse = (targetVerse, bookId, chapter, verse, groupsIndex = null, invalidateCheckCallback = null) => {
   let filtered = null;
@@ -40,6 +43,9 @@ export const validateAllSelectionsForVerse = (targetVerse, bookId, chapter, vers
     if (isEqual(reference, checkingOccurrence.contextId?.reference)) {
       if (selections && selections.length) {
         if (!filtered) { // for performance, we filter the verse only once and only if there is a selection
+          if (typeof targetVerse !== 'string') {
+            targetVerse = getUsfmForVerseContent(targetVerse)
+          }
           filtered = usfm.removeMarker(targetVerse); // remove USFM markers
         }
 
@@ -57,12 +63,12 @@ export const validateAllSelectionsForVerse = (targetVerse, bookId, chapter, vers
 
 /**
  * verify all selections
- * @param {string} targetVerse - new text for verse
+ * @param {Object} targetBible - target bible
  * @param {Object[]} groupsIndex - array of all the checks
  * @param {Function} invalidateCheckCallback
- * @return {Function}
+ * @return {Promise<boolean>}
  */
-export const validateAllSelections = (targetVerse, groupsIndex = null, invalidateCheckCallback = null) => {
+export async function validateAllSelections(targetBible,groupsIndex = null, invalidateCheckCallback = null)  {
   let _selectionsChanged = false;
 
   for (let j = 0, lenGI = groupsIndex.length; j < lenGI; j++) {
@@ -70,16 +76,26 @@ export const validateAllSelections = (targetVerse, groupsIndex = null, invalidat
     const selections = checkingOccurrence.selections;
 
     if (selections && selections.length) {
-      const filtered = usfm.removeMarker(targetVerse); // remove USFM markers
+      const reference = checkingOccurrence.reference
+      const chapter = reference.chapter
+      const verse = reference.verse
+      const ref = `${chapter}:${verse}`
+      let targetVerse = getVerses(targetBible, ref);
+      if (targetVerse) {
+        if (typeof targetVerse !== 'string') {
+          targetVerse = getUsfmForVerseContent(targetVerse)
+        }
+        const filtered = usfm.removeMarker(targetVerse) // remove USFM markers
 
-      const selectionsChanged = validateVerseSelections(filtered, selections);
-      if (selectionsChanged) {
-        invalidateCheckCallback && invalidateCheckCallback(checkingOccurrence)
-        _selectionsChanged = true
+        const selectionsChanged = validateVerseSelections(filtered, selections)
+        if (selectionsChanged) {
+          invalidateCheckCallback && invalidateCheckCallback(checkingOccurrence)
+          _selectionsChanged = true
+        }
+        await delay(10)
       }
     }
   }
 
   return _selectionsChanged
-};
-
+}
